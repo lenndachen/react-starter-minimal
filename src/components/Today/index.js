e73b7c43ca52d4e17276718b757921caad11a538
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-11-25 14:30:25 -0500

    Update index.js

--- src/components/Today/index.js
+++ src/components/Today/index.js
@@ -1,27 +1,52 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCloud, faSun, faCloudRain, faSnowflake } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCloud,
+  faSun,
+  faCloudRain,
+  faSnowflake
+} from "@fortawesome/free-solid-svg-icons";
 library.add(faSun, faCloud, faCloudRain, faSnowflake);
 
 class Today extends React.Component {
   getBigIcon() {
     const newArray1 = this.props.dayInfo;
-    console.log("newArray1", this.props.dayInfo)
-      if (newArray1 === "Rain") {
-        return <div><FontAwesomeIcon icon={faCloudRain} size="xs" /></div>;
-      }
-      if (newArray1 === "Clouds") {
-        return <div><FontAwesomeIcon icon={faCloud} size="xs"/></div>;
-      }
-      if (newArray1 === "Sunny") {
-        return <div><FontAwesomeIcon icon={faSun} size="xs"/></div>;
-    };
+    console.log("newArray1", this.props.dayInfo);
+    if (newArray1 === "Rain") {
+      return (
+        <div>
+          <FontAwesomeIcon icon={faCloudRain} size="xs" />
+        </div>
+      );
+    }
+    if (newArray1 === "Clouds") {
+      return (
+        <div>
+          <FontAwesomeIcon icon={faCloud} size="xs" />
+        </div>
+      );
+    }
+    if (newArray1 === "Sunny") {
+      return (
+        <div>
+          <FontAwesomeIcon icon={faSun} size="xs" />
+        </div>
+      );
+    }
     if (newArray1 === "Clear") {
-      return <div><FontAwesomeIcon icon={faSun} size="xs" /></div>;
+      return (
+        <div>
+          <FontAwesomeIcon icon={faSun} size="xs" />
+        </div>
+      );
     }
     if (newArray1 === "Snow") {
-      return <div><FontAwesomeIcon icon={faSnowflake} size="xs" /></div>;
+      return (
+        <div>
+          <FontAwesomeIcon icon={faSnowflake} size="xs" />
+        </div>
+      );
     }
   }
 
@@ -32,24 +57,21 @@ class Today extends React.Component {
     return solution;
   }
 
-
-
   render() {
     return (
       <div className="today">
         <div className="temp-div">
-          {this.props.temperature && <div className="temp-wrapper">
-            {this.getTemp(this.props.temperature)}
-                <span className="the-f-span">℉</span>
-          </div>}
+          {this.props.temperature && (
+            <div className="temp-wrapper">
+              {this.getTemp(this.props.temperature)}
+              <span className="the-f-span">℉</span>
+            </div>
+          )}
           <div className="today-time-and-icon-div">
-          <div className="big-icon">{this.getBigIcon()}</div>
-        </div>
-
+            <div className="big-icon">{this.getBigIcon()}</div>
           </div>
-    </div>
-      
-
+        </div>
+      </div>
     );
   }
 }
